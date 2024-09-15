893e4138466f7951ff15f877a696732c88fdca61
Leo-Dev-Full-Stack <leo.devfullstack@gmail.com>
2024-09-15 15:24:31 -0300

    adicionando as funções da biblioteca

---
+++ calculadoraBiblioteca.h
@@ -0,0 +1,15 @@
+int somar(int n1, int n2) {
+    return n1+n2;
+}
+int subdtrair(int n1, int n2) {
+    return n1-n2;
+}
+int multiplicar(int n1, int n2) {
+    return n1*n2;
+}
+int dividir(int n1, int n2) {
+    return n1/n2;
+}
+int modulo(int n1, int n2) {
+    return n1%n2;
+}
